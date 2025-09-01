c5dc4889d9761ab22ceb62902358c5913963ad91
XiuweiZhang01 <167188360+XiuweiZhang01@users.noreply.github.com>
2025-09-01 11:36:39 +0000

    Deploying to gh-pages from @ XiuweiZhang01/XiuweiZhang01.github.io@7ce3a2d6fa47a13180f730a358a4f3401fa6cf34 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -418,16 +418,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+            },},{id: "news-become-an-undergraduate-student-in-electronic-engineering-at-the-university-of-central-lancashire",
+          title: 'Become an undergraduate student in Electronic Engineering at the University of Central Lancashire....',
+          description: "",
+          section: "News",},{id: "news-announcement-2",
+          title: 'Announcement_2',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-become-an-undergraduate-student-in-electronic-engineering-at-the-university-of-central-lancashire",
-          title: 'Become an undergraduate student in Electronic Engineering at the University of Central Lancashire....',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
